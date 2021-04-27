1ef0d2a352d901dfe2147742b5f67391364da073
jialihong <jialihong1992>
2021-04-27 17:07:50 +0800

    初始架构  spring-cloud-feign

---
+++ shop-platform/src/main/java/com/amity/shop/shopplatform/ShopPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.amity.shop.shopplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopPlatformApplication.class, args);
+    }
+
+}
---
+++ shop-platform/src/test/java/com/amity/shop/shopplatform/ShopPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amity.shop.shopplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
